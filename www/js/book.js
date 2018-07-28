137d75d0ffb66e92967d0fba2c6141eadbf734bf
s41173 <sanjaya.kiran@gmail.com>
2018-07-28 12:26:46 +0700

    vibration

--- www/js/book.js
+++ www/js/book.js
@@ -5,6 +5,7 @@ function vibrator(){
   navigator.vibrate([1000, 1000, 2000, 1000, 2000, 1000, 2000, 1000, 2000]);
 }
 
+get_ongoing_order();
 vibrator();
 cek_order_active();
 var vibrate = setInterval(function(){ vibrator(); }, 20000);
@@ -17,6 +18,29 @@ function cek_order_active(){
                 toast(data.error);
                 setTimeout(function(){ window.location = "index.html"; }, 3000);
             }
+        });
+    }
+}
+
+function get_ongoing_order(){
+    if (localStorage.userid != undefined && localStorage.log != undefined){
+        $.get(api+"sales/cek_ongoing", function(data, status){
+                var datax = data.content[0];
+                $("#ordercode").html(datax.code);
+                $("#orderaddress").html(datax.destination);
+                $("#orderpayment").html(datax.payment_type);
+        });
+    }
+}
+
+function book(){
+    if (localStorage.userid != undefined && localStorage.log != undefined){
+        $.get(api+"sales/book_order/"+localStorage.sid, function(data, status){
+           if (data.status == true){
+            localStorage.removeItem("sid");
+            toast(data.error);
+            setTimeout(function(){ window.location = "ongoing.html"; }, 2000);
+           }
         });
     }
 }
