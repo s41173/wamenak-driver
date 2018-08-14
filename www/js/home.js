bf538417c5d4505296f462c503090e9f59227d4c
s41173 <sanjaya.kiran@gmail.com>
2018-08-14 12:35:28 +0700

    sleep1

--- www/js/home.js
+++ www/js/home.js
@@ -1,9 +1,10 @@
-sleep(1);
+
+setTimeout(function(){ sleep(1); }, 10000);
 function sleep(val){
     if (val == 1){ 
-      window.plugins.insomnia.keepAwake(); 
+    //   window.plugins.insomnia.keepAwake(); 
       toast('Screen Light On'); }else{
-      window.plugins.insomnia.allowSleepAgain(); 
+    //   window.plugins.insomnia.allowSleepAgain(); 
       toast('Screen Light Off');
     }
 }
